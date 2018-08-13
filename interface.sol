ebb238981b50c517c20fb8039d91a4b01d5e1bb4
Rakesh Narang <rakesh.narang@nagarro.com>
2018-08-13 15:05:11 +0530

    video 3: custom modifiers and error handling

--- interface.sol
+++ interface.sol
@@ -8,16 +8,23 @@ interface Regulator {
 contract Bank is Regulator {
     
     uint private value;
+	address private owner;
     
     function Bank(uint amount) {
         value = amount;
+		owner = msg.sender;
     }
+	
+	modifier ownerFunc {
+		require(owner == msg.sender);
+		_;
+	}
     
-    function deposit (uint amount) {
+    function deposit (uint amount) private ownerFunc {
         value += amount;
     }
     
-    function balance() returns (uint) {
+    function balance() returns (uint) internal ownerFunc {
         return value;
     }
     
@@ -57,4 +64,23 @@ contract MyFirstContract is Bank(100) {
     function getAge() returns (uint) {
         return age;
     }
+}
+
+
+contract TestThrows {
+	function testAssert() {
+		assert(false);
+	}
+	
+	function testRequire() {
+		require(1==2);
+	}
+	
+	function testRevert() {
+		revert();
+	}
+	
+	function testThrow() {
+		throw;
+	}
 }
